5eeb626e8ac075caca929b2fc9781ac9a85a36c4
viniciius14 <rodri.f.ribeiro@gmail.com>
2022-11-23 11:01:31 +0000

    commit

--- DNS Server/dns_server.c
+++ DNS Server/dns_server.c
@@ -6,7 +6,7 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
-#include "../Headers/dns.h"
+#include "../Headers/dns_utils.h"
 
 
 
@@ -20,44 +20,74 @@
 
 
 
+// This funcion will create our dns packet for replys
+void create_packet(struct Message_Query* Q_ptr, struct Message_Response* R_ptr)
+{
 
+    unsigned char new_rdata[] = {0xac, 0xd9, 0xa8, 0xae};
 
+    struct Header new_header = {
+        .ID         = Q_ptr->header.ID,   // Varies everytime
+        .QR         = 0x01,     // It's a response
+        .OPCODE     = 0x00,     // Standard query (0)
+        .AA         = 0x00,     // Server is not an authority for domain
+        .TC         = 0x00,     // Message is not truncated
+        .RD         = 0x01,     // Do query recursively
+        .RA         = 0x01,     // Server can do recursive queries
+        .Z          = 0x00,
+        //0x00                  // Answer authenticated: Answer/authority portion was not authenticated by the server
+        //0x00                  // Non-authenticated data: Unacceptable
+        .RCODE      = 0x00,     // No error
+        .QDCOUNT    = 0x01,     // Questions: 1
+        .ANCOUNT    = 0x01,     // Resource records in answer section
+        .NSCOUNT    = 0x00,     // Server resource records in the authority records section
+        .ARCOUNT    = 0x01      // Resource records in the additional records section
+    };
+    struct Question new_question = {
+        .QNAME      = Q_ptr->question.QNAME,
+        .QTYPE      = 0x01,     // Type A
+        .QCLASS     = 0x01      // Class: IN (0x0001)
+    };
+    struct Resource new_answer = {
+        .NAME       = Q_ptr->question.QNAME,
+        .TYPE       = 0x01,         // Type A
+        .CLASS      = 0x01,         // Class: IN (0x0001)
+        .TTL        = 0x54,         // 84 seconds
+        .RDLENGTH   = 0x04,         // length of RDATA
+        .RDATA      = new_rdata  // 4 octet ARPA Internet address
+    };
+    struct Resource new_additional = {
+        .NAME       = 0x00,     // <Root>
+        .TYPE       = 0x29,     // OPT 41
+        .CLASS      = 0x0200,   // UDP payload size
+        //0x00                  // "Higher bits in extended RCODE: 0x00"
+        //EDNS0 version: 0
+        .TTL        = 0x00,     // Not specified
+        .RDLENGTH   = 0x0c,     // Length of RDATA
+        .RDATA      = 0x00      // Not specified
+    };
 
+    R_ptr->header = new_header;
+    R_ptr->question = new_question; 
+    R_ptr->answer = new_answer; 
+    R_ptr->additional = new_additional;
 
+    //printf("%x\n%s\n",R_ptr->header.ID, R_ptr->question.QNAME);
 
-
-// // This funcion will create our dns packet for replys
-// struct Answer* create_packet(struct Query* ptr)
-// {
-
-//     struct Answer ans = {
-//         .ID = ptr->ID,
-//         .flags = {
-//             .RESPONSE = 1,
-//             .OPCODE = 0,
-//             .
-//         }
-
-//     }
-   
-
-
-
-
-
-//     return 
-// }
+}
 
 
 
 
 int main(void){
 
+    struct Message_Response* test = (struct Message_Response*)malloc(sizeof(struct Message_Response));
+
     struct Message_Query SGQ = {
         .header = {
             .ID         = 0x700b,   // Varies everytime
             .QR         = 0x00,     // It's a query
-            .Opcode     = 0x00,     // Standard query (0)
+            .OPCODE     = 0x00,     // Standard query (0)
             .AA         = 0x00,     // Only for responses
             .TC         = 0x00,     // Message is not truncated
             .RD         = 0x01,     // Do query recursively
@@ -86,7 +116,14 @@ int main(void){
             .RDATA      = 0x00      // Not specified
         }
     };
-    
+
+
+    struct Message_Query* ptr = &SGQ;
+
+
+
+
+    create_packet(ptr, test);
 
     
 
--- Headers/dns_utils.h
+++ Headers/dns_utils.h
@@ -3,65 +3,65 @@
 
 #include <stdint.h>
 
-// struct __attribute__((__packed__)) Header
-// {
-//     __uint16_t ID;      
-//     __uint16_t QR : 1;
-//     __uint16_t Opcode : 4;
-//     __uint16_t AA : 1;
-//     __uint16_t TC : 1;
-//     __uint16_t RD : 1;
-//     __uint16_t RA : 1;
-//     __uint16_t Z : 3;
-//     __uint16_t RCODE : 4;
-//     __uint16_t QDCOUNT;
-//     __uint16_t ANCOUNT;
-//     __uint16_t NSCOUNT;
-//     __uint16_t ARCOUNT;
-// };
+struct __attribute__((__packed__)) Header
+{
+    __uint16_t ID;      
+    __uint16_t QR : 1;
+    __uint16_t OPCODE : 4;
+    __uint16_t AA : 1;
+    __uint16_t TC : 1;
+    __uint16_t RD : 1;
+    __uint16_t RA : 1;
+    __uint16_t Z : 3;
+    __uint16_t RCODE : 4;
+    __uint16_t QDCOUNT;
+    __uint16_t ANCOUNT;
+    __uint16_t NSCOUNT;
+    __uint16_t ARCOUNT;
+};
 
-// struct __attribute__((__packed__)) Question
-// {
-//     char *QNAME;
-//     __uint16_t QTYPE;
-//     __uint16_t QCLASS;
-// };
+struct __attribute__((__packed__)) Question
+{
+    char *QNAME;
+    __uint16_t QTYPE;
+    __uint16_t QCLASS;
+};
 
-// struct __attribute__((__packed__)) Resource
-// {
-//     char *NAME;
-//     __uint16_t TYPE;
-//     __uint16_t CLASS;
-//     __uint32_t TTL;
-//     __uint16_t RDLENGTH;
-//     char *RDATA;
-// };
+struct __attribute__((__packed__)) Resource
+{
+    char *NAME;
+    __uint16_t TYPE;
+    __uint16_t CLASS;
+    __uint32_t TTL;
+    __uint16_t RDLENGTH;
+    char *RDATA;
+};
 
-// struct __attribute__((__packed__)) Message_Query
-// {
-//     struct Header header;
-//     struct Question question;
-//     struct Resource additional;
-// };
+struct __attribute__((__packed__)) Message_Query
+{
+    struct Header header;
+    struct Question question;
+    struct Resource additional;
+};
 
 
-// struct __attribute__((__packed__)) Message_Reply
+struct __attribute__((__packed__)) Message_Response
+{
+    struct Header header;
+    struct Question question;
+    struct Resource answer;
+    struct Resource additional;
+};
+
+// // "The answer, authority, and additional sections all share the same format"
+// struct __attribute__((__packed__)) Message
 // {
 //     struct Header header;
 //     struct Question question;
 //     struct Resource answer;
+//     struct Resource authority;
 //     struct Resource additional;
 // };
-
-// // // "The answer, authority, and additional sections all share the same format"
-// // struct __attribute__((__packed__)) Message
-// // {
-// //     struct Header header;
-// //     struct Question question;
-// //     struct Resource answer;
-// //     struct Resource authority;
-// //     struct Resource additional;
-// // };
 
 
 
@@ -69,100 +69,130 @@
 
 // All in accordance with wireshark
 // Same for response and query
-struct __attribute__((__packed__)) Queries
-{
-    unsigned char* NAME; 
-    __uint16_t TYPE;
-    __uint16_t CLASS;
-};
+// struct __attribute__((__packed__)) Queries
+// {
+//     unsigned char* NAME; 
+//     __uint16_t TYPE;
+//     __uint16_t CLASS;
+// };
 
-// Query 
-struct __attribute__((__packed__)) Q_Flags
-{
-    __uint16_t RESPONSE:1;
-    __uint16_t OPCODE:4;
-    __uint16_t TRUNCATED:1;
-    __uint16_t RECURSION:1;
-    __uint16_t Z:1;
-    __uint16_t AD:1;
-    __uint16_t NON_AUTH:1;
-};
+// // Query 
+// struct __attribute__((__packed__)) Q_Flags
+// {
+//     __uint16_t RESPONSE:1;
+//     __uint16_t OPCODE:4;
+//     __uint16_t TRUNCATED:1;
+//     __uint16_t RECURSION:1;
+//     __uint16_t Z:1;
+//     __uint16_t AD:1;
+//     __uint16_t NON_AUTH:1;
+// };
 
-struct __attribute__((__packed__)) Q_Additional_Records
-{
-    __uint16_t ADD_NAME:8;
-    __uint16_t OPT;
-    __uint16_t UDP_PAYLOAD;
-    __uint16_t RCODE:8;
-    __uint16_t EBNS0:8;
-    __uint16_t Z;
-    __uint16_t LENGTH;
-};
+// struct __attribute__((__packed__)) Q_Additional_Records
+// {
+//     __uint16_t ADD_NAME:8;
+//     __uint16_t OPT;
+//     __uint16_t UDP_PAYLOAD;
+//     __uint16_t RCODE:8;
+//     __uint16_t EBNS0:8;
+//     __uint16_t Z;
+//     __uint16_t LENGTH;
+// };
 
-struct __attribute__((__packed__)) Q_Option_Cookie
-{
-    __uint16_t OPT_CODE;
-    __uint16_t OPT_LENGTH;
-    unsigned long OPT_DATA;
-};
+// struct __attribute__((__packed__)) Q_Option_Cookie
+// {
+//     __uint16_t OPT_CODE;
+//     __uint16_t OPT_LENGTH;
+//     unsigned long OPT_DATA;
+// };
 
-struct __attribute__((__packed__)) Query    //final struct to be used in the program
-{
-    __uint16_t ID;
-    struct Q_Flags Flags;
-    __uint16_t QUESTIONS;
-    __uint16_t ANSWER_RRS;
-    __uint16_t AUTHORITY_RR;
-    __uint16_t ADDITIONAL_RR;
-    struct Queries Queries;
-    struct Q_Additional_Records Add_Records;
+// struct __attribute__((__packed__)) Query    //final struct to be used in the program
+// {
+//     __uint16_t ID;
+//     struct Q_Flags Flags;
+//     __uint16_t QUESTIONS;
+//     __uint16_t ANSWER_RRS;
+//     __uint16_t AUTHORITY_RR;
+//     __uint16_t ADDITIONAL_RR;
+//     struct Queries Queries;
+//     struct Q_Additional_Records Add_Records;
 
-};
-
-
+// };
 
 
 
 
-// Response
-struct __attribute__((__packed__)) R_Flags
-{
-    __uint16_t RESPONSE:1;          // Set to 1 since it's a response
-    __uint16_t OPCODE:4;
-    __uint16_t AUTHORITATIVE:1;
-    __uint16_t TRUNCATED:1;
-    __uint16_t RECURSION_DESIRED:1;
-    __uint16_t RECURSION_AVAILABLE:1;
-    __uint16_t Z:1;
-    __uint16_t ANSWER_AUTHENTICATED:1;
-    __uint16_t NON_AUTH:1;
-    __uint16_t REPLY_CODE:4;        // 0 if no error
-};
-
-struct __attribute__((__packed__)) R_Answers
-{
-    unsigned char* NAME;
-    __uint16_t TYPE;
-    __uint16_t CLASS;
-    __uint32_t TTL;
-    __uint16_t DATA_LENGTH;
-    __uint32_t ADDRESS;
-};
 
 
+// // Response
+// struct __attribute__((__packed__)) R_Flags
+// {
+//     __uint16_t RESPONSE:1;          // Set to 1 since it's a response
+//     __uint16_t OPCODE:4;
+//     __uint16_t AUTHORITATIVE:1;
+//     __uint16_t TRUNCATED:1;
+//     __uint16_t RECURSION_DESIRED:1;
+//     __uint16_t RECURSION_AVAILABLE:1;
+//     __uint16_t Z:1;
+//     __uint16_t ANSWER_AUTHENTICATED:1;
+//     __uint16_t NON_AUTH:1;
+//     __uint16_t REPLY_CODE:4;        // 0 if no error
+// };
 
-struct __attribute__((__packed__)) Response
-{
-    __uint16_t ID;
-    struct R_Flags Flags;
-    __uint16_t QUESTIONS;
-    __uint16_t ANSWER_RRS;
-    __uint16_t AUTHORITY_RR;
-    __uint16_t ADDITIONAL_RR;
-    struct Queries Queries;
-    struct R_Answers* Answers;  // Will be a list with size ANSWER_RRS 
-};
+// struct __attribute__((__packed__)) R_Answers
+// {
+//     unsigned char* NAME;
+//     __uint16_t TYPE;
+//     __uint16_t CLASS;
+//     __uint32_t TTL;
+//     __uint16_t DATA_LENGTH;
+//     __uint32_t ADDRESS;
+// };
+
+
+
+// struct __attribute__((__packed__)) Response
+// {
+//     __uint16_t ID;
+//     struct R_Flags Flags;
+//     __uint16_t QUESTIONS;
+//     __uint16_t ANSWER_RRS;
+//     __uint16_t AUTHORITY_RR;
+//     __uint16_t ADDITIONAL_RR;
+//     struct Queries Queries;
+//     struct R_Answers Answers[];  // Will be a list with size ANSWER_RRS 
+// };
 
 
 
 #endif
+
+
+// struct Response ans = {
+//     .ID = ptr->ID,
+//     .Flags = {
+//         .RESPONSE = 1,
+//         .OPCODE = 0,
+//         .AUTHORITATIVE = 0,
+//         .TRUNCATED = 0,
+//         .RECURSION_DESIRED = 1,
+//         .RECURSION_AVAILABLE = 1,
+//         .Z = 0,
+//         .ANSWER_AUTHENTICATED = 0,
+//         .NON_AUTH = 0,
+//         .REPLY_CODE = 0
+//     },
+//     .QUESTIONS = 1,
+//     .ANSWER_RRS = 1,
+//     .AUTHORITY_RR = 0,
+//     .ADDITIONAL_RR = 1,
+//     .Queries = {
+//         .NAME = ptr->Queries.NAME,
+//         .TYPE = 1,
+//         .CLASS = 1
+//     },
+//     .Answers = {
+//         {
+
+//         }
+//     }
