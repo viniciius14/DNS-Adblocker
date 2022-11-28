e24d94d99473db9c56c2a8fe65ea0187b170eeaf
viniciius14 <rodri.f.ribeiro@gmail.com>
2022-11-28 11:07:38 +0000

    commit

---
+++ DNS Server/dns.c
@@ -0,0 +1,129 @@
+#include "Headers/dns_utils.h"
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main(void){
+
+    struct Message_Query buf;
+
+    if(await_receive(buf) != 0)
+    {
+        exit(1);
+    } 
+
+    struct Message_Response SGA = {
+        .header = {
+            .ID         = 0x700b,   // varies everytime
+            .QR         = 0x01,     // It's a response
+            .OPCODE     = 0x00,     // Standard query (0)
+            .AA         = 0x00,     // Server is not an authority for domain
+            .TC         = 0x00,     // Message is not truncated
+            .RD         = 0x01,     // Do query recursively
+            .RA         = 0x01,     // Server can do recursive queries
+            .Z          = 0x00,
+            //0x00                  // Answer authenticated: Answer/authority portion was not authenticated by the server
+            //0x00                  // Non-authenticated data: Unacceptable
+            .RCODE      = 0x00,     // No error
+            .QDCOUNT    = 0x01,     // Questions: 1
+            .ANCOUNT    = 0x01,     // Resource records in answer section
+            .NSCOUNT    = 0x00,     // Server resource records in the authority records section
+            .ARCOUNT    = 0x01      // Resource records in the additional records section
+        },
+        .question = {
+            .QNAME      = "googlecom",
+            .QTYPE      = 0x01,     // Type A
+            .QCLASS     = 0x01      // Class: IN (0x0001)
+        },
+        .answer = {
+            .NAME       = "googlecom",
+            .TYPE       = 0x01,         // Type A
+            .CLASS      = 0x01,         // Class: IN (0x0001)
+            .TTL        = 0x54,         // 84 seconds
+            .RDLENGTH   = 0x04,         // length of RDATA
+            .RDATA      = "0xacd9a8ae"    // 4 octet ARPA Internet address
+        },
+        .additional = {
+                .NAME       = 0x00,     // <Root>
+                .TYPE       = 0x29,     // OPT 41
+                .CLASS      = 0x0200,   // UDP payload size
+                //0x00                  // "Higher bits in extended RCODE: 0x00"
+                //EDNS0 version: 0
+                .TTL        = 0x00,     // Mot specified
+                .RDLENGTH   = 0x0c,     // Length of RDATA
+                .RDATA      = 0x00      // Mot specified
+            }
+    };
+    //char reply[100];
+    //create_reply(buffer, reply);
+
+
+
+
+    if(dns_send(SGA, sizeof(SGA)) != 0){
+        exit(1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// void* last;
+
+// printf("sizeof array   -----> %ld\n",sizeof(reply));
+
+
+// for(int i = 0 ; i != sizeof(reply) ; i++){
+//     printf("%p -----> %x \n", &(reply[i]), reply[i]);
+//     last = &reply[i];
+// }
+
+// foo(reply, sizeof(reply));
+
+
+
+// void *foo(unsigned char *reply, size_t size){
+//     void* last;
+
+//     printf("sizeof array   -----> %ld\n",sizeof(reply));/// not the actual size, sizeof pointer
+
+
+//     for(int i = 0 ; i != size; i++){
+//         printf(" -----> %x \n",  reply[i]);
+//         last = &reply[i];
+//     }
+
+//     unsigned char n_reply[size];
+
+//     printf("\n\n\n");
+//     for(int i = 0 ; i != size ; i++){
+// 		n_reply[i] = reply[i];
+// 	}
+
+//     for(int i = 0 ; i != size; i++){
+//         printf(" -----> %x \n", n_reply[i]);
+//     }
+
+// }
+
+
+
--- Test Client/client.c
+++ Test Client/client.c
@@ -1,114 +1,80 @@
-/*
-** talker.c -- a datagram "client" demo
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
+#include "Headers/dns_utils.h"
 #include <unistd.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-
-#define SERVERPORT "4950"	// the port users will be connecting to
-#define MAXBUFLEN 100
-
-int main(int argc, char *argv[])
-{
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-
-	unsigned char packet[] = {
-		0x5f, 0xa6, 0x01, 0x20, 0x00,
-		0x01, 0x00, 0x00, 0x00, 0x00, 
-		0x00, 0x01, 0x06, 0x67, 0x6f, 
-		0x6f, 0x67, 0x6c, 0x65, 0x03, 
-		0x63, 0x6f, 0x6d, 0x00, 0x00,
-		0x01, 0x00, 0x01, 0x00, 0x00,
-		0x29, 0x10, 0x00, 0x00, 0x00, 
-		0x00, 0x00, 0x00, 0x0c, 0x00, 
-		0x0a, 0x00, 0x08, 0x45, 0x40,
-		0xa5, 0x9a, 0x21, 0x27, 0x2f, 0x48
-	};
-
-
-	// if (argc != 3) {
-	// 	fprintf(stderr,"usage: talker hostname \n");
-	// 	exit(1);
-	// }
-
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
-	hints.ai_socktype = SOCK_DGRAM;
-
-	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
-	// loop through all the results and make a socket
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("talker: socket");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) {
-		fprintf(stderr, "talker: failed to create socket\n");
-		return 2;
-	}
-
-	if ((numbytes = sendto(sockfd, packet, sizeof(packet), 0,
-			 p->ai_addr, p->ai_addrlen)) == -1) {
-		perror("talker: sendto");
-		exit(1);
-	}
-
-	//freeaddrinfo(servinfo);
-
-	printf("talker: sent %d bytes to %s\n", numbytes, argv[1]);
 
 
 
-	socklen_t addr_len;
-	struct sockaddr_storage their_addr;
-	char buf[MAXBUFLEN];
-	char s[INET6_ADDRSTRLEN];
 
-	addr_len = sizeof their_addr;
-	if ((numbytes = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
-		(struct sockaddr *)&their_addr, &addr_len)) == -1) {
-		perror("recvfrom");
-		exit(1);
-	}	
 
-	printf("client: got packet from %s\n", 
-        inet_ntop(their_addr.ss_family, 
-        get_in_addr((struct sockaddr *)&their_addr), s, sizeof s));
-    
-	printf("client: packet is %d bytes long\n", numbytes);
-	buf[numbytes] = '\0';
-	printf("client: packet contains: ");
 
-    for(int i = 0 ; i != numbytes ; i++){
-        printf("%c",buf[i]);
+
+
+
+int main(void){
+    struct Message_Query SGQ = {
+        .header = {
+            .ID         = 0x700b,   // Varies everytime
+            .QR         = 0x00,     // It's a query
+            .OPCODE     = 0x00,     // Standard query (0)
+            .AA         = 0x00,     // Only for responses
+            .TC         = 0x00,     // Message is not truncated
+            .RD         = 0x01,     // Do query recursively
+            .RA         = 0x00,     // Only for responses
+            .Z          = 0x00,
+                        //0x01      // AD bit: Set (not sure what this is)
+            .RCODE      = 0x00,     // Only for responses
+            .QDCOUNT    = 0x01,     // Questions: 1
+            .ANCOUNT    = 0x00,     // Resource records in answer section
+            .NSCOUNT    = 0x00,     // Server resource records in the authority records section
+            .ARCOUNT    = 0x01      // Resource records in the additional records section
+        },
+        .question = {
+            .QNAME  = "googlecom",
+            .QTYPE  = 0x01,         // Type A
+            .QCLASS = 0x01          // Class: IN (0x0001)
+        },
+        .additional = {
+            .NAME       = 0x00,     // <Root>
+            .TYPE       = 0x29,     // OPT 41
+            .CLASS      = 0x1000,   // UDP payload size
+            //0x00                  // "Higher bits in extended RCODE: 0x00"
+            //EDNS0 version: 0
+            .TTL        = 0x00,     // Not specified
+            .RDLENGTH   = 0x0c,     // Length of RDATA
+            .RDATA      = 0x00      // Not specified
+        }
+    };
+
+
+
+    unsigned char reply[] = {
+        0xc0, 0x32, 0x81, 0x80, 0x00,
+        0x01, 0x00, 0x01, 0x00, 0x00,
+        0x00, 0x01, 0x06, 0x67, 0x6f,
+        0x6f, 0x67, 0x6c, 0x65, 0x03,
+        0x63, 0x6f, 0x6d, 0x00, 0x00, 
+        0x01, 0x00, 0x01, 0xc0, 0x0c, 
+        0x00, 0x01, 0x00, 0x01, 0x00, 
+        0x00, 0x00, 0x73, 0x00, 0x04, 
+        0x8e, 0xfa, 0xb8, 0x0e, 0x00, 
+        0x00, 0x29, 0x02, 0x00, 0x00, 
+        0x00, 0x00, 0x00, 0x00, 0x00
+    };
+
+
+    if(dns_send(SGQ, sizeof(struct Message_Query)) != 0)
+    {
+        exit(1);
     }
-    printf("\n");
 
 
 
-	freeaddrinfo(servinfo);
 
-	close(sockfd);
 
-	return 0;
+    char buffer[100];
+    if(await_receive(buffer) != 0)
+    {
+        exit(1);
+    } 
+
 }
+
