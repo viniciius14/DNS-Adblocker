5fa844bc19cde9e1ca2afb4fff7867134f991b7a
viniciius14 <rodri.f.ribeiro@gmail.com>
2022-11-28 11:26:12 +0000

    commit

--- DNS Server/dns.c
+++ DNS Server/dns.c
@@ -12,55 +12,55 @@ int main(void){
         exit(1);
     } 
 
-    struct Message_Response SGA = {
-        .header = {
-            .ID         = 0x700b,   // varies everytime
-            .QR         = 0x01,     // It's a response
-            .OPCODE     = 0x00,     // Standard query (0)
-            .AA         = 0x00,     // Server is not an authority for domain
-            .TC         = 0x00,     // Message is not truncated
-            .RD         = 0x01,     // Do query recursively
-            .RA         = 0x01,     // Server can do recursive queries
-            .Z          = 0x00,
-            //0x00                  // Answer authenticated: Answer/authority portion was not authenticated by the server
-            //0x00                  // Non-authenticated data: Unacceptable
-            .RCODE      = 0x00,     // No error
-            .QDCOUNT    = 0x01,     // Questions: 1
-            .ANCOUNT    = 0x01,     // Resource records in answer section
-            .NSCOUNT    = 0x00,     // Server resource records in the authority records section
-            .ARCOUNT    = 0x01      // Resource records in the additional records section
-        },
-        .question = {
-            .QNAME      = "googlecom",
-            .QTYPE      = 0x01,     // Type A
-            .QCLASS     = 0x01      // Class: IN (0x0001)
-        },
-        .answer = {
-            .NAME       = "googlecom",
-            .TYPE       = 0x01,         // Type A
-            .CLASS      = 0x01,         // Class: IN (0x0001)
-            .TTL        = 0x54,         // 84 seconds
-            .RDLENGTH   = 0x04,         // length of RDATA
-            .RDATA      = "0xacd9a8ae"    // 4 octet ARPA Internet address
-        },
-        .additional = {
-                .NAME       = 0x00,     // <Root>
-                .TYPE       = 0x29,     // OPT 41
-                .CLASS      = 0x0200,   // UDP payload size
-                //0x00                  // "Higher bits in extended RCODE: 0x00"
-                //EDNS0 version: 0
-                .TTL        = 0x00,     // Mot specified
-                .RDLENGTH   = 0x0c,     // Length of RDATA
-                .RDATA      = 0x00      // Mot specified
-            }
-    };
+    // struct Message_Response SGA = {
+    //     .header = {
+    //         .ID         = 0x700b,   // varies everytime
+    //         .QR         = 0x01,     // It's a response
+    //         .OPCODE     = 0x00,     // Standard query (0)
+    //         .AA         = 0x00,     // Server is not an authority for domain
+    //         .TC         = 0x00,     // Message is not truncated
+    //         .RD         = 0x01,     // Do query recursively
+    //         .RA         = 0x01,     // Server can do recursive queries
+    //         .Z          = 0x00,
+    //         //0x00                  // Answer authenticated: Answer/authority portion was not authenticated by the server
+    //         //0x00                  // Non-authenticated data: Unacceptable
+    //         .RCODE      = 0x00,     // No error
+    //         .QDCOUNT    = 0x01,     // Questions: 1
+    //         .ANCOUNT    = 0x01,     // Resource records in answer section
+    //         .NSCOUNT    = 0x00,     // Server resource records in the authority records section
+    //         .ARCOUNT    = 0x01      // Resource records in the additional records section
+    //     },
+    //     .question = {
+    //         .QNAME      = "googlecom",
+    //         .QTYPE      = 0x01,     // Type A
+    //         .QCLASS     = 0x01      // Class: IN (0x0001)
+    //     },
+    //     .answer = {
+    //         .NAME       = "googlecom",
+    //         .TYPE       = 0x01,         // Type A
+    //         .CLASS      = 0x01,         // Class: IN (0x0001)
+    //         .TTL        = 0x54,         // 84 seconds
+    //         .RDLENGTH   = 0x04,         // length of RDATA
+    //         .RDATA      = "0xacd9a8ae"    // 4 octet ARPA Internet address
+    //     },
+    //     .additional = {
+    //             .NAME       = 0x00,     // <Root>
+    //             .TYPE       = 0x29,     // OPT 41
+    //             .CLASS      = 0x0200,   // UDP payload size
+    //             //0x00                  // "Higher bits in extended RCODE: 0x00"
+    //             //EDNS0 version: 0
+    //             .TTL        = 0x00,     // Mot specified
+    //             .RDLENGTH   = 0x0c,     // Length of RDATA
+    //             .RDATA      = 0x00      // Mot specified
+    //         }
+    // };
     //char reply[100];
     //create_reply(buffer, reply);
 
 
 
 
-    if(dns_send(&SGA, sizeof(SGA)) != 0){
+    if(dns_send(buf, sizeof(buf)) != 0){
         exit(1);
     }
 }
