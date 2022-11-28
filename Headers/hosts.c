bffe1f8186baf23475d060241d9709355b51d7e9
Rodrigo Ribeiro <rodri.f.ribeiro@gmail.com>
2022-11-28 17:25:28 +0000

    commit

--- Headers/hosts.c
+++ Headers/hosts.c
@@ -1,8 +1,27 @@
 #include "hosts.h"
 
-FILE *fp;
+struct Line{
+    int ip1, ip2, ip3, ip4;
+    char *host_name;
+};
 
 
-fp = fopen("hosts")
 
 
+void read_line(char *host){
+
+    FILE *fp = fopen("../Hosts/hosts.txt","r");
+
+
+    struct Line line;
+    char buffer[250];
+    fgets(buffer, sizeof(buffer), fp);
+    if(buffer[0] != '#'){
+        sscanf(buffer, "%.d.%d.%d.%d %[^\n] \n", line.ip1, line.ip2, line.ip3, line.ip4, line.host_name);
+    }
+    else{
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+
+}
--- Headers/hosts.h
+++ Headers/hosts.h
@@ -2,9 +2,12 @@
 #define HOSTS_H
 
 #include <stdio.h>
-
+void read_line(char *host);
 //ler a linha %.d.%d.%d.%d %s
 //se a linha comecar com um # ignorar
 
+//estrutura da cache
+// 0.0.0.0 %s datetime ttl 
+//must support addittion and addition of new lines
 
 #endif
